5022a1b103c8188aff1d0cfbdc6478012880a8a3
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-03-20 21:54:23 -0500

    test

--- javascript-examples/randomNumberFunction.js
+++
@@ -1,3 +0,0 @@
-const randomize = (min, max) => Math.round(Math.random() * (max - min)) + min;
-
-console.log(randomize(5, 20)); // output: 5-20
--- javascript-examples/remainderValue.js
+++
@@ -1,7 +0,0 @@
-for (let i = 0; i < 11; i++) {
-  if (i % 5 === 0) {
-    console.log(i, "Jeremy is 💥");
-  } else {
-    console.log(i, "Woof");
-  }
-}
